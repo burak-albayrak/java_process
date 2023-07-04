b428895947e93da41cebfc1b5aaaea5b9aba8634
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-05 01:47:04 +0300

    Variables

---
+++ java_syntax/_1_Variables/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        int number = 5;
+        double dnumber = 5.5;
+        char character = 'A';
+        boolean is_it_true = false;
+
+        number = 55;
+
+        System.out.println(number);
+        System.out.println(dnumber);
+        System.out.println(character);
+        System.out.println(is_it_true);
+    }
+}
