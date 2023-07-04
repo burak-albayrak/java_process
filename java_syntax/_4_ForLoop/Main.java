4295abb98c15ee1f0d18aa61dc81e2929b94b79c
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-05 01:51:53 +0300

    ForLoop

---
+++ java_syntax/_4_ForLoop/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        for (int i = 2; i < 10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Loop is over");
+    }
+}
