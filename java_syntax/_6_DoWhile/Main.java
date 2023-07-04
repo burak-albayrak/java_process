d8a768b6f4049c3e30ab82a6f282618012bf24de
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-05 01:53:25 +0300

    DoWhile

---
+++ java_syntax/_6_DoWhile/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+        int i = 2;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i < 10);
+        System.out.println("Loop is over");
+    }
+}
