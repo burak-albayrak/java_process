807a7df1a0d51b94df4fa08de0c96dbe82c4797d
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-05 01:47:58 +0300

    If

---
+++ java_syntax/_2_If/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+        int number = 55;
+        if (number < 10){
+            System.out.println("nE");
+        } else if (number == 55) {
+            System.out.println("Samsun");
+        } else {
+            System.out.println("Mal");
+        }
+    }
+}
