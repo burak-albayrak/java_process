24c247f37b8dfe4ee7b19486013e6adcbfd51f81
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-10 21:15:35 +0300

    Main.java

---
+++ java_labs/lab1/q2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(-10 + 38);
+        System.out.println(85/5);
+        System.out.println(12*9);
+        System.out.println(49-76);
+        System.out.println(102%8);
+    }
+}
